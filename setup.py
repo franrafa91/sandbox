024b10e2ec9ac65df6a3da8f21483fe7cdfe610c
coespino93 <coespino@syr.edu>
2024-09-27 12:22:20 -0400

    initial library set up

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sandbox_fc',
+    packages=find_packages(include=['sandbox_fc']),
+    version='0.1.0',
+    description='Sandbox FC',
+    author='Carlos Espinoza, Rafael Reyes',
+)
